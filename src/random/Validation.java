bb92933232ee1559b7d2b16b8ecbb4035d013873
akelbaz <league@WTS22.attlocal.net>
2019-02-17 11:31:27 -0800

    MODULE 2
    UP TO MAGIC 8 BALL IN ECLIPSE

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,17 +9,27 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+
 		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+		for (int i = 0; i < 10; i++) {
+			int randomNumber = randomMaker.nextInt(5);
+			System.out.println(randomNumber);
+			// 2. Repeat all the code above 10 times
+			if (randomNumber == 0) {
+				JOptionPane.showMessageDialog(null, "ME LIKE UR HAIR");
+			} else if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "YOU WON A FREE CRUISE");
+			} else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "UR MY SPECIAL FRIEND");
+			} else if (randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "SOMETHING NICE");
+			} else if (randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "I HAVE NOTHING ELSE NICE TO SAY");
 
-		// 2. Repeat all the code above 10 times
-		
-		// 3. Find someone to test out your program. They will like it :)
+			}
+			// 3. Find someone to test out your program. They will like it :)
+		}
 	}
 }
